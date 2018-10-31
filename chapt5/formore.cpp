431104426319a7917cf6fc7a5a9015f0b0682211
kuangwenyi <kuangwenyi@MacBook-Pro-2.local>
2018-10-31 12:45:31 +0800

    move formore.cpp

--- chapt5/formore.cpp
+++ chapt5/formore.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 const int arrSize = 16;
 int main(){
-    using namespace std;
     long long factorials[arrSize];
     factorials[0] = factorials[1] = 1LL;
     for (int idx = 2; idx < arrSize ; ++idx){
-        factorials[idx] = idx * factorials[idx - 1] ;
+        factorials[idx] += factorials[idx-1];
     }
     for (int idx = 0; idx < arrSize ; ++idx){
         cout << "we are iterating the " << idx <<"! = " << factorials[idx] << endl;
